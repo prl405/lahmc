4fdb9076b06882b5dc177cec19456d8760ad09b4
prl405 <73357564+prl405@users.noreply.github.com>
2024-06-20 12:04:44 +0100

    - Adjusted autocorrelation calculation to match the original implmentation

--- run_lahmc.jl
+++ run_lahmc.jl
@@ -4,6 +4,7 @@ using Statistics
 
 include("lahmc.jl")
 
+global dU_count = 0
 
 function U_rough_well(X, scale1, scale2)
     cosX = cos.(X * 2 * pi / scale2)
@@ -22,44 +23,20 @@ function U(X)
 end
 
 function dU(X)
+    global dU_count += 1
     return dU_rough_well(X, theta[1], theta[2])
 end
 
-# function calc_corr(samples)
-# 	"""
-# 	Calculate autocorrelation given history.  Assumes 0 mean.
-# 	"""
-
-# 	T = len(samples)
-# 	N = samples[1]['X'].shape[0]
-# 	# nbatch = samples[0]['X'].shape[1]
-
-# 	X = zeros((N,nbatch,T))
-# 	for tt in 1:T
-# 		X[:,:,tt] = samples[tt]['X']
-#     end
-# 	c = zeros((T-1,))
-# 	c[1] = mean(X.^2)
-# 	for t_gap in 1:T-1
-# 		c[t_gap] = mean(X[:,:,:-t_gap]*X[:,:,t_gap:])
-#     end
-# 	return c/c[0]
-# end
-
 function calculate_autocorrelation(samples)
-    n_samples = size(samples, 2)
-    mean_samples = mean(samples, dims=2)
-    samples_centered = samples .- mean_samples
-    acf = zeros(Float64, size(samples, 1), n_samples - 1)
-    
-    for i in 1:size(samples, 1)
-        for lag in 1:n_samples-1
-            acf[i, lag] = mean(samples_centered[i, 1:end-lag] .* samples_centered[i, lag+1:end])
-        end
-        acf[i, :] /= var(samples[i, :])
+    n_samples = length(samples[1,:])
+    acf = zeros(n_samples)
+    acf[1] = mean(samples.^2)
+
+    for lag in 2:(n_samples-1)
+        acf[lag] = mean(vcat(samples[:, 1:end-lag] .* samples[:, lag+1:end]...))
     end
     
-    return acf
+    return acf/acf[1]
 end
 
 theta = [100, 4]
@@ -67,7 +44,7 @@ epsilon = 1
 L = 10
 K = 4
 beta = 1
-n_samples = 500
+n_samples = 5000
  
 DataSize = 2
 X_init = randn(DataSize)*theta[1]
@@ -79,14 +56,10 @@ plt_trace = plot(transpose(samples), xlabel="Sample", ylabel="Chain value", titl
 display(plt_trace)
 print("Acceptance Rate: ", acceptRate)
 
-# Calculate autocorrelation
 autocorrelation = calculate_autocorrelation(samples)
-avg_autocorrelation = mean(autocorrelation, dims=1)
 
-# Calculate gradient evaluations
-# gradient_evaluations = dU_count
+gradient_evaluations = collect(0:(dU_count/(length(autocorrelation)-1)):dU_count)
 
-# Plot
-plt_ac = plot(1:n_samples-1, avg_autocorrelation', title="Gradient Evaluations vs Autocorrelation", xlabel="Sample", ylabel="Autocorrelation")
+plt_ac = plot(gradient_evaluations, autocorrelation, title="Gradient Evaluations vs Autocorrelation", xlabel="Gradient Evaluations", ylabel="Autocorrelation")
 
 display(plt_ac)
