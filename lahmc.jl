1606778b1c3a84f4c175933033716e8f9747e20f
prl405 <73357564+prl405@users.noreply.github.com>
2024-06-30 13:45:02 +0100

    - Error free version of batching lahmc - Still WIP

--- lahmc.jl
+++ lahmc.jl
@@ -1,117 +1,113 @@
-
-
-function lahmc(U, dU, init_q, epsilon, L, K, beta, n_samples)
-    samples = zeros(length(init_q), n_samples)
-	samples[:, 1] = init_q
-	N = length(init_q)
-    # current_q = init_q
-	p = randn(N)
-    accept_count = 0
-
-    for i in 1:n_samples
-		Z_chain = i==1 ? [init_q] : [samples[:, i-1]]
-        proposed_p = NaN
-		# use the same random number for comparison for the entire chain
-		rand_comparison = rand(1)
-		# the current cumulative probability of acceptance
-		p_cum = 0
-		# the cumulative probability matrix, so we only need to visit each leaf once when recursing
-		C = ones((K+1, K+1))*NaN
-		# the current set of indices for samples that have not yet been accepted for a transition
-		# active_idx = collect(range(nbatch))
-        
-        for j in 1:K
-			proposed_q, proposed_p = leapfrog(Z_chain[end], p, epsilon, L, dU)
-			push!(Z_chain, proposed_q)
-
-			# recursively calculate the cumulative probability of doing this many leaps
-			p_cum, Cl = leap_prob_recurse(Z_chain, C[1:j+1, 1:j+1], U)
-			C[1:j+1, 1:j+1] = Cl
-			# find all the samples that did this number of leaps, and update self.state with them
-			accept = p_cum >= rand_comparison[1]
-			if accept
-				accept_count += 1
-				# current_q = Z_chain[end]
-				break
-			end
-
-			# flip the momenutm for any samples that were unable to place elsewhere
-			if j == K
-				p = -proposed_p
-			end
-
-			# counter['L%d'%(j+1)] += len(accepted_idx)
-			# state.update(accepted_idx, Z_chain[-1])
-			# update the set of active indices, so we don't do simulate trajectories for samples that are already assigned to a state
-			# active_idx = active_idx[p_cum.vec() < rand_comparison[active_idx]]
-			# if length(active_idx) == 0
-			# 	break
-            # end
-			# Z_chain[-1].active_idx = active_idx
-        end
-		
-		# counter['F'] += len(active_idx)
-		# if self.display > 1:
-		# 	print("Transition counts "),
-		# 	for k in sorted(self.counter.keys()):
-		# 		print ("%s:%d"%(k, self.counter[k])),
-		# corrupt the momentum
-		p = p*sqrt(1-beta) + randn(N)*sqrt(beta)
-		# state.update_EV()
-        
-        samples[:, i] = Z_chain[end]
-    end
-
-    acceptRate = accept_count / n_samples
-    return samples, acceptRate
+mutable struct LAHMC
+    U::Function
+    dU::Function
+    epsilon::Float64
+    L::Int
+    K::Int
+    beta::Float64
+    n_param::Int
+    n_samples::Int
+	n_batch::Int
+    samples::Array{Float64}
+    accept_count::Int
+    dU_count::Int
 end
 
-function leap_prob_recurse(Z_chain, C, U)
+function LAHMC(U, dU, init_q, epsilon, L, K, beta, n_samples)
+	n_param = size(init_q, 1)
+    n_batch = size(init_q, 2)
+    samples = fill(NaN, n_param, n_batch, n_samples)
+    samples[:, :, 1] = init_q
+    LAHMC(U, dU, epsilon, L, K, beta, n_param, n_samples, n_batch, samples, 0, 0)
+end
+
+function call_dU!(lahmc::LAHMC)
+    lahmc.dU_count += 1
+    return lahmc.dU
+end
+
+function sample!(lahmc::LAHMC)
+    p = randn(lahmc.n_param, lahmc.n_batch)
+
+    for i in 1:lahmc.n_samples-1
+        Z_chain = [lahmc.samples[:, :, i]]
+        rand_comparison = rand(1, lahmc.n_batch)
+        p_cum = zeros(1, n_batch)
+        C = fill(NaN, lahmc.K+1, lahmc.K+1, lahmc.n_batch)
+        active_idx = collect(1:lahmc.n_batch)
+        
+        for j in 1:lahmc.K
+            proposed_q, p = leapfrog(Z_chain[end], p, lahmc.epsilon, lahmc.L, call_dU!(lahmc))
+            push!(Z_chain, proposed_q)
+            
+            p_cum, Cl = leap_prob_recurse(Z_chain, C[1:j+1, 1:j+1, active_idx], active_idx, lahmc.U)
+            C[1:j+1, 1:j+1, active_idx] = Cl
+            
+            accepted_idx = active_idx[vcat(p_cum...) .>= rand_comparison[active_idx]]
+            lahmc.accept_count += length(accepted_idx)
+            lahmc.samples[:, accepted_idx, i+1] = Z_chain[end][:, accepted_idx]
+
+            active_idx = active_idx[vcat(p_cum...) .< rand_comparison[active_idx]]
+
+			if length(active_idx) == 0
+				break
+            end
+        end
+
+        p[:, active_idx] = -p[:, active_idx]
+        lahmc.samples[:, active_idx, i+1] = lahmc.samples[:, active_idx, i]
+
+        p = p * sqrt(1 - lahmc.beta) + randn(lahmc.n_param, lahmc.n_batch) * sqrt(lahmc.beta)
+        
+    end
+end
+
+function leap_prob_recurse(Z_chain, C, active_idx, U)
 	"""
 	Recursively compute to cumulative probability of transitioning from
 	the beginning of the chain Z_chain to the end of the chain Z_chain.
 	"""
-	if !isnan(C[1,end])
+	if !isnan(C[1,end, 1])
 		# we've already visited this leaf
-		cumu = C[1,end]
-		return cumu, C
+		cumu = C[1,end, :]
+		return cumu', C
     end
+    
 	if length(Z_chain) == 2
 		# the two states are one apart
-		H0 = U(Z_chain[1]) - 0.5 * sum(Z_chain[1].^2)
-    	H1 = U(Z_chain[2]) - 0.5 * sum(Z_chain[2].^2)
-		p_acc = exp(H0 - H1)
-		# p_acc = p_acc[:,active_idx]
+		H0 = U(Z_chain[1]) .- 0.5 * sum(Z_chain[1].^2, dims=1)
+    	H1 = U(Z_chain[2]) .- 0.5 * sum(Z_chain[2].^2, dims=1)
+        diff = H0 .- H1
+        p_acc = ones((1, size(diff, 2)))
+		p_acc[diff.<0] = exp.(diff[diff.<0]) 
+		p_acc = p_acc[:, active_idx]
 		#print C.shape, C[0,-1,:].shape, p_acc.shape, p_acc.ravel().shape
-		C[1,end] = p_acc
+		C[1,end, :] = vcat(p_acc...)
 		return p_acc, C
     end
 
-	cum_forward, Cl = leap_prob_recurse(Z_chain[1:end-1], C[1:end-1, 1:end-1], U)
-	C[1:end-1,1:end-1] = Cl
-	cum_reverse, Cl = leap_prob_recurse(Z_chain[end:-1:2], C[end:-1:2, end:-1:2], U)
-	C[end:-1:2, end:-1:2] = Cl
+	cum_forward, Cl = leap_prob_recurse(Z_chain[1:end-1], C[1:end-1, 1:end-1, :], active_idx, U)
+	C[1:end-1,1:end-1, :] = Cl
+	cum_reverse, Cl = leap_prob_recurse(Z_chain[end:-1:2], C[end:-1:2, end:-1:2, :], active_idx, U)
+	C[end:-1:2, end:-1:2, :] = Cl
 
-	H0 = U(Z_chain[1]) - 0.5 * sum(Z_chain[1].^2)
-    H1 = U(Z_chain[end]) - 0.5 * sum(Z_chain[end].^2)
-	start_state_ratio = exp(H0 - H1)
+	H0 = U(Z_chain[1]) .- 0.5 * sum(Z_chain[1].^2, dims=1)
+    H1 = U(Z_chain[end]) .- 0.5 * sum(Z_chain[end].^2, dims=1)
+    diff = H0 .- H1
+    diff = diff[:, active_idx]
+	start_state_ratio = exp.(diff)
 
-	prob = min(1 .- cum_forward, start_state_ratio*(1 .- cum_reverse))
+	prob = minimum(vcat(1 .- cum_forward, start_state_ratio.*(1 .- cum_reverse)), dims=1)
 	cumu = cum_forward + prob
-	C[1,end] = cumu
+	C[1,end,: ] = vcat(cumu...)
 
 	return cumu, C
 end
 
 function leapfrog(current_q, current_p, epsilon, L, dU)
-
-	p = current_p
-
-    # Initial half-step for momentum
-    p = p .- 0.5 * epsilon * dU(current_q)
-
-    # Full steps for position and momentum
-    proposed_q = copy(current_q)
+    p = current_p .- 0.5 * epsilon .* dU(current_q)
+    proposed_q = current_q
+    
     for i in 1:L
         proposed_q = proposed_q .+ epsilon .* p
         if i < L
@@ -121,6 +117,6 @@ function leapfrog(current_q, current_p, epsilon, L, dU)
 
     # Final half-step for momentum
     proposed_p = p .- 0.5 * epsilon .* dU(proposed_q)
-	
+
     return proposed_q, proposed_p
 end
--- run_lahmc.jl
+++ run_lahmc.jl
@@ -4,11 +4,9 @@ using Statistics
 
 include("lahmc.jl")
 
-global dU_count = 0
-
 function U_rough_well(X, scale1, scale2)
     cosX = cos.(X * 2 * pi / scale2)
-    E = sum((X.^2) / (2 * scale1^2) .+ cosX)
+    E = sum((X.^2) ./ (2 * scale1^2) .+ cosX, dims=1)
     return E
 end
 
@@ -18,48 +16,46 @@ function dU_rough_well(X, scale1, scale2)
     return dEdX
 end
 
-function U(X)
-    return U_rough_well(X, theta[1], theta[2])
-end
-
-function dU(X)
-    global dU_count += 1
-    return dU_rough_well(X, theta[1], theta[2])
-end
-
 function calculate_autocorrelation(samples)
-    n_samples = length(samples[1,:])
+    n_samples = size(samples, 3)
     acf = zeros(n_samples)
     acf[1] = mean(samples.^2)
-
+    
     for lag in 2:(n_samples-1)
-        acf[lag] = mean(vcat(samples[:, 1:end-lag] .* samples[:, lag+1:end]...))
+        acf[lag] = mean(vcat(samples[:, :, 1:end-lag] .* samples[:, :, lag+1:end]...))
     end
     
-    return acf/acf[1]
+    return acf / acf[1]
 end
 
+# Example usage
+Random.seed!(1234)
 theta = [100, 4]
 epsilon = 1
 L = 10
 K = 4
 beta = 1
-n_samples = 5000
- 
+n_samples = 500
+
 DataSize = 2
-X_init = randn(DataSize)*theta[1]
+n_batch = 100
+X_init = randn(DataSize, n_batch) * theta[1]
 
-samples, acceptRate = lahmc(U, dU, X_init, epsilon, L, K, beta, n_samples)
+U(X) = U_rough_well(X, theta[1], theta[2])
+dU(X) = dU_rough_well(X, theta[1], theta[2])
 
-plt_trace = plot(transpose(samples), xlabel="Sample", ylabel="Chain value", title="Trace plot")
+lahmc = LAHMC(U, dU, X_init, epsilon, L, K, beta, n_samples)
+sample!(lahmc)
+
+plt_trace = plot(transpose(lahmc.samples[:,1,:]), xlabel="Sample", ylabel="Chain value", title="Trace plot")
 
 display(plt_trace)
-print("Acceptance Rate: ", acceptRate)
+print("Acceptance Rate: ", lahmc.accept_count/(lahmc.n_samples*lahmc.n_batch))
 
-autocorrelation = calculate_autocorrelation(samples)
+# autocorrelation = calculate_autocorrelation(lahmc.samples)
 
-gradient_evaluations = collect(0:(dU_count/(length(autocorrelation)-1)):dU_count)
+# gradient_evaluations = collect(0:(lahmc.dU_count/(length(autocorrelation)-1)):lahmc.dU_count)
 
-plt_ac = plot(gradient_evaluations, autocorrelation, title="Gradient Evaluations vs Autocorrelation", xlabel="Gradient Evaluations", ylabel="Autocorrelation")
+# plt_ac = plot(gradient_evaluations, autocorrelation, title="Gradient Evaluations vs Autocorrelation", xlabel="Gradient Evaluations", ylabel="Autocorrelation")
 
-display(plt_ac)
+# display(plt_ac)
