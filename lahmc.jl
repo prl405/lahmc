7e9c129342b81380810e3cb4204e2aad5ef766f6
prl405 <73357564+prl405@users.noreply.github.com>
2024-07-02 18:11:23 +0100

    - Single sampler where rejected samples are not added to the final sample list

--- lahmc.jl
+++ lahmc.jl
@@ -9,8 +9,8 @@ function lahmc(U, dU, init_q, epsilon, L, K, beta, n_samples)
     accept_count = 0
 
     for i in 1:n_samples
-		Z_chain = i==1 ? [init_q] : [samples[:, i-1]]
-        proposed_p = NaN
+		q_chain = i==1 ? [init_q] : [samples[:, i-1]]
+		p_chain = [p]
 		# use the same random number for comparison for the entire chain
 		rand_comparison = rand(1)
 		# the current cumulative probability of acceptance
@@ -21,45 +21,30 @@ function lahmc(U, dU, init_q, epsilon, L, K, beta, n_samples)
 		# active_idx = collect(range(nbatch))
         
         for j in 1:K
-			proposed_q, proposed_p = leapfrog(Z_chain[end], p, epsilon, L, dU)
-			push!(Z_chain, proposed_q)
+			proposed_q, proposed_p = leapfrog(q_chain[end], p, epsilon, L, dU)
+			push!(q_chain, proposed_q)
+			push!(p_chain, proposed_p)
 
 			# recursively calculate the cumulative probability of doing this many leaps
-			p_cum, Cl = leap_prob_recurse(Z_chain, C[1:j+1, 1:j+1], U)
+			p_cum, Cl = leap_prob_recurse(q_chain, C[1:j+1, 1:j+1], U)
 			C[1:j+1, 1:j+1] = Cl
 			# find all the samples that did this number of leaps, and update self.state with them
 			accept = p_cum >= rand_comparison[1]
 			if accept
 				accept_count += 1
-				# current_q = Z_chain[end]
+				samples[:, i] = q_chain[end]
 				break
 			end
 
 			# flip the momenutm for any samples that were unable to place elsewhere
-			if j == K
+			if (j == K) & (!accept)
 				p = -proposed_p
+				samples[:, i] = q_chain[1]
 			end
-
-			# counter['L%d'%(j+1)] += len(accepted_idx)
-			# state.update(accepted_idx, Z_chain[-1])
-			# update the set of active indices, so we don't do simulate trajectories for samples that are already assigned to a state
-			# active_idx = active_idx[p_cum.vec() < rand_comparison[active_idx]]
-			# if length(active_idx) == 0
-			# 	break
-            # end
-			# Z_chain[-1].active_idx = active_idx
         end
 		
-		# counter['F'] += len(active_idx)
-		# if self.display > 1:
-		# 	print("Transition counts "),
-		# 	for k in sorted(self.counter.keys()):
-		# 		print ("%s:%d"%(k, self.counter[k])),
 		# corrupt the momentum
-		p = p*sqrt(1-beta) + randn(N)*sqrt(beta)
-		# state.update_EV()
-        
-        samples[:, i] = Z_chain[end]
+		p = p*sqrt(1-beta) + randn(N)*sqrt(beta)  
     end
 
     acceptRate = accept_count / n_samples
